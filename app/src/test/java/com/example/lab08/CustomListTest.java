c070aaafad3864922b34274531b76548e957e11c
ilozue <ilozue@ualberta.ca>
2025-10-31 13:53:00 -0600

    Failing test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,22 @@
-package com.example.lab08;
+package com.example.lab08; // <-- match your app's actual package
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    private CustomList makeEmptyList() {
+        return new CustomList(null, new java.util.ArrayList<City>());
+    }
+
+    @Test
+    public void testHasCity() {
+        CustomList list = makeEmptyList();
+        City calgary = new City("Calgary", "AB");
+
+        assertFalse(list.hasCity(calgary));
+
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+    }
 }
